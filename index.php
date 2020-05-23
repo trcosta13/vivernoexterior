ef412703fc92486ad98b1e3340001f522f91eaf5
Thiago Costa <25648241+trcosta13@users.noreply.github.com>
2020-05-22 22:08:50 -0400

    Adicionado Título e Meta Tags

--- index.php
+++ index.php
@@ -2,7 +2,14 @@
 	<html lang="pt-br">
 
 		<head>
-
+			<title>Viver no Exterior - Seu Ponto de Referência no Exterior</title>
+			<meta charset="utf-8" />
+			<meta name="description" content="O Seu Ponto de Referência no Exterior" />
+			<meta name="keywords" content="Empregos, USA, Boston, Florida, Empresa, Serviços, Oportunidades, Trabalhos, 
+			EUA, Emprega Boston, Comunidade, Jobs, Empresarial, Craigslist, Vaga, Carpenter, Painter, Landscape, Housecleaner, Housekeeper, Eventos, Classificados" />
+			<meta name="author" content="Imaggine IT Solutions" />
+			<meta name="robots" content="index, follow" />
+			<meta name="revisit-after" content="5 days" />
 		</head>
 
 		<body>
